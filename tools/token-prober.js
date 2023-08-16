a8f4e17ed86d4d2ead042571a2cae3d5ebb7e808
Menersar <menersar@menersar.com>
2023-08-16 07:58:05 +0200

    Update token-prober.js

--- tools/token-prober.js
+++ tools/token-prober.js
@@ -14,7 +14,9 @@ const SCRATCH_API = 'https://api.scratch.mit.edu/projects/$id';
 // const TRAMPOLINE_API = 'https://trampoline.turbowarp.org/proxy/projects/$id';
 // const TRAMPOLINE_API = 'https://mixality.github.io/Sidekick/trampoline/proxy/projects/$id';
 const TRAMPOLINE_API = 'https://menersar.github.io/Sidekick/trampoline/proxy/projects/$id';
+// !!! NEU
 const LOCALHOST_API = 'http://localhost:8080/proxy/projects/$id';
+// const LOCALHOST_API = 'http://0.0.0.0:8080/proxy/projects/$id';
 
 const run = async (url) => {
   console.log(`Using ${url} with ID ${ID}`);
